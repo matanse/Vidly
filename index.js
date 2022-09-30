4386d61e49d2ce05865dc7d57c3c86179b6c13d7
unknown <matan.selig@gmail.com>
2022-09-30 04:29:57 +0200

    add middleware morgan, helmet, urlencoded

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 const Joi = require("joi");
+const helmet = require("helmet");
+const morgan = require("morgan");
 const express = require("express");
 const app = express();
 const logger = require("./logger");
 const authenticator = require("./authenticator");
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(helmet());
+if (app.get("env") === "development") {
+  app.use(morgan("tiny"));
+  console.log("Morgan is enabled...");
+}
+
 // Self made middleware
 app.use(logger);
 app.use(authenticator);
